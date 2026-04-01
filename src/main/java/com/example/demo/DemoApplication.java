de4258da6d95a29711866bedeb8bf7236039e750
23CS101 <23CS101@licet.com>
2026-04-01 09:09:01 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
+
+@RestController
+@RequestMapping("/student")
+class StudentController {
+
+    @GetMapping("/greet")
+    public String greetStudent() {
+        return "Hello Student! Welcome to the system.";
+    }
+
+
+    @GetMapping("/greet/{name}")
+    public String greetByName(@PathVariable String name) {
+        return "Hello " + name + "! Welcome to the Student Greeting System.";
+    }
+}
